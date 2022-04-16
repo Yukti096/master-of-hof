bfd01c335ecfc3e8bda93d6991a35b5d0d5e33c9
Yukti Sharma <70809051+Yukti096@users.noreply.github.com>
2022-04-17 00:55:43 +0530

    yukti-8911

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import HigherOrderComponent from './higherordercomponents';
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderComponent/>
+    </div>
+  );
+}
+
+export default App;
